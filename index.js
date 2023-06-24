0f9b6dc0d88176e87efae3302a744d8bd414553b
Narayan2208 <narayandutta2208@gmail.com>
2023-06-24 14:32:09 +0530

    set implement

--- index.js
+++ index.js
@@ -23,3 +23,34 @@ function findTwoSumWithPointers(array, target) {
 
     return output;
 }
+
+//! The time complexity of the updated findTwoSum function using the two-pointer approach is O(n log n), where n is the length of the input array.
+
+
+// ? Function to find pairs of numbers in an array that add up to a target value (With set method)
+function findTwoSumWithSet(array, target) {
+    const output = []; //*  I Initialize an empty array to store the output pairs
+    const set = new Set(); //*  Initialize an empty set to store the visited numbers
+
+    for (let i = 0; i < array.length; i++) {
+        const isPresent = target - array[i]; //*  Calculate the  current number
+
+        if (set.has(isPresent)) { //* If the isPresent exists in the set, it means we have found a pair
+            output.push([array[i], isPresent]);  // Add the pair to the output array
+        } else {
+            set.add(array[i]); //*  Add the current number to the set for future reference
+        }
+    }
+
+    return output; //* return the array of pairs
+}
+
+//! The time complexity of the findTwoSumWithSet function using the set method is O(n), where n is the length of the input array.
+//! This implementation has a more efficient time complexity compared to the previous implementation using the two-pointer approach, which had a time complexity of O(n log n) due to the sorting operation.
+
+
+const array = [1, 3, 2, 2, -4, -6, -2, 8];
+const target = 4;
+
+const output = findTwoSumWithSet(array, target);
+console.log("First Combination For “4” : "); console.log(output);
