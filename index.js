39cad8572251655d4bee086b122111e949b69504
Narayan2208 <narayandutta2208@gmail.com>
2023-06-24 12:55:08 +0530

    find targeted value complete

---
+++ index.js
@@ -0,0 +1,23 @@
+function findTwoSum(array, target) {
+    const output = [];
+    const set = new Set();
+
+    for (let i = 0; i < array.length; i++) {
+        const complement = target - array[i];
+
+        if (set.has(complement)) {
+            output.push([array[i], complement]);
+        } else {
+            set.add(array[i]);
+        }
+    }
+
+    return output;
+}
+
+const array = [1, 3, 2, 2, -4, -6, -2, 8];
+const target = 4;
+
+const output = findTwoSum(array, target);
+console.log(output);
+
